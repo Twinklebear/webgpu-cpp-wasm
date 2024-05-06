ffcdc8e43b9c30397711b9e5ffd9d4a54c9661a9
Will Usher <will@willusher.io>
2024-05-05 18:09:57 -0700

    change the screen color on web builds

--- src/main.cpp
+++ src/main.cpp
@@ -10,9 +10,19 @@
 #include <emscripten/html5_webgpu.h>
 #endif
 
+#include <webgpu/webgpu_cpp.h>
+
 #include "embedded_files.h"
 
 struct AppState {
+    wgpu::Instance instance;
+    wgpu::Device device;
+    wgpu::Queue queue;
+
+    wgpu::Surface surface;
+    wgpu::SwapChain swap_chain;
+    wgpu::RenderPipeline render_pipeline;
+
     ArcballCamera camera;
     glm::mat4 proj;
 
@@ -44,7 +54,49 @@ int main(int argc, const char **argv)
                                           0);
     std::cout << "SDL window = " << window << "\n";
 
-    // TODO: set up webgpu device
+    app_state->instance = wgpu::CreateInstance();
+
+// TODO: set up webgpu device
+#ifdef EMSCRIPTEN
+    app_state->device = wgpu::Device::Acquire(emscripten_webgpu_get_device());
+#else
+    // Need to use request adapter here
+#endif
+
+    app_state->device.SetUncapturedErrorCallback(
+        [](WGPUErrorType type, const char *msg, void *data) {
+            std::cout << "WebGPU Error: " << msg << "\n" << std::flush;
+#ifdef EMSCRIPTEN
+            emscripten_cancel_main_loop();
+            emscripten_force_exit(1);
+#endif
+            std::exit(1);
+        },
+        nullptr);
+
+    app_state->queue = app_state->device.GetQueue();
+
+#ifdef EMSCRIPTEN
+    wgpu::SurfaceDescriptorFromCanvasHTMLSelector selector;
+    selector.selector = "#canvas";
+#else
+#error "TODO"
+#endif
+
+    wgpu::SurfaceDescriptor surface_desc;
+    surface_desc.nextInChain = &selector;
+
+    app_state->surface = app_state->instance.CreateSurface(&surface_desc);
+
+    wgpu::SwapChainDescriptor swap_chain_desc;
+    swap_chain_desc.format = wgpu::TextureFormat::BGRA8Unorm;
+    swap_chain_desc.usage = wgpu::TextureUsage::RenderAttachment;
+    swap_chain_desc.presentMode = wgpu::PresentMode::Fifo;
+    swap_chain_desc.width = win_width;
+    swap_chain_desc.height = win_height;
+
+    app_state->swap_chain =
+        app_state->device.CreateSwapChain(app_state->surface, &swap_chain_desc);
 
 #ifdef EMSCRIPTEN
     emscripten_set_main_loop_arg(app_loop, app_state, -1, 0);
@@ -69,4 +121,27 @@ void app_loop(void *_app_state)
             app_state->done = true;
         }
     }
+
+    wgpu::RenderPassColorAttachment color_attachment;
+    color_attachment.view = app_state->swap_chain.GetCurrentTextureView();
+    color_attachment.clearValue.r = 0.f;
+    color_attachment.clearValue.g = 0.f;
+    color_attachment.clearValue.b = 1.f;
+    color_attachment.clearValue.a = 1.f;
+    color_attachment.loadOp = wgpu::LoadOp::Clear;
+    color_attachment.storeOp = wgpu::StoreOp::Store;
+
+    wgpu::RenderPassDescriptor pass_desc;
+    pass_desc.colorAttachmentCount = 1;
+    pass_desc.colorAttachments = &color_attachment;
+
+    wgpu::CommandEncoder encoder = app_state->device.CreateCommandEncoder();
+
+    // Just clear the image
+    wgpu::RenderPassEncoder render_pass_enc = encoder.BeginRenderPass(&pass_desc);
+    render_pass_enc.End();
+
+    wgpu::CommandBuffer commands = encoder.Finish();
+    // Here the # refers to the number of command buffers being submitted
+    app_state->queue.Submit(1, &commands);
 }
