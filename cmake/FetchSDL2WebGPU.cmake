06f724a8133bc39c0460097cbc1a97e3804eceee
Will Usher <will@willusher.io>
2024-05-25 14:41:41 -0700

    formatting

--- cmake/FetchSDL2WebGPU.cmake
+++ cmake/FetchSDL2WebGPU.cmake
@@ -3,7 +3,6 @@ include(FetchContent)
 FetchContent_Declare(
   sdl2webgpu
   GIT_REPOSITORY git@github.com:Twinklebear/sdl2webgpu.git
-  GIT_TAG main
-)
+  GIT_TAG main)
 
 FetchContent_MakeAvailable(sdl2webgpu)
