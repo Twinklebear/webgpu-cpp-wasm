c88f0713700fc10745e88ff4db2abaa249447430
Will Usher <will@willusher.io>
2024-08-17 20:16:28 -0700

    Claenup build and fix debug build

--- cmake/tbb.cmake
+++ cmake/tbb.cmake
@@ -4,15 +4,12 @@ ExternalProject_Add(
   tbb_build
   PREFIX tbb_build
   GIT_REPOSITORY https://github.com/oneapi-src/oneTBB.git
-  GIT_TAG 2d516c871b79d81032f0ca44d76e5072bc62d479
-  # GIT_SHALLOW ON We need to run the emcmake cmake wrapper to build
-  CMAKE_COMMAND $<$<BOOL:${EMSCRIPTEN}>:emcmake>
-  ${CMAKE_COMMAND}
-  # Emscripten build flags for TBB from
-  # https://github.com/oneapi-src/oneTBB/blob/master/WASM_Support.md
-  CMAKE_ARGS -DTBB_STRICT=OFF
+  GIT_TAG v2021.13.0
+  GIT_SHALLOW ON
+  CMAKE_ARGS -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}
+             -DCMAKE_CROSSCOMPILING_EMULATOR=${CMAKE_CROSSCOMPILING_EMULATOR}
+             -DTBB_STRICT=OFF
              -DCMAKE_CXX_FLAGS=-Wno-unused-command-line-argument
-             $<$<BOOL:${EMSCRIPTEN}>:-DTBB_DISABLE_HWLOC_AUTOMATIC_SEARCH=ON>
              -DBUILD_SHARED_LIBS=ON
              -DTBB_EXAMPLES=OFF
              -DTBB_TEST=OFF
@@ -24,6 +21,8 @@ ExternalProject_Get_Property(tbb_build INSTALL_DIR)
 target_include_directories(tbb INTERFACE "${INSTALL_DIR}/include")
 
 if(EMSCRIPTEN)
+  # The emscripten build says "build shared libs", but emscripten will always
+  # output static libs
   target_link_libraries(
     tbb
     INTERFACE
